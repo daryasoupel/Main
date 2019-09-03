6c3972eafd8cc2d65547e061ab7ebe59312aab27
daryasovpel <dariawoow56@gmail.com>
2019-09-03 13:40:56 +0300

    пЕРЕМЕН. INT

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello, world");
+        int i;
+        i =10+2*3;
+        System.out.println("i=" +i);
     }
 }
