2bc5ae48df3895a16809f417f2a071724b2575a5
daryasovpel <dariawoow56@gmail.com>
2019-09-03 16:06:26 +0300

    String

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args){
-        int i;
-        int j = 5;
-        i =10+2*3;
-        j =j+1;
-        System.out.println("i=" +i);
-        System.out.println("j=" +j);
-        System.out.println((i == 16)? "i equal 16" : "i NOT equal 16");
+        int x = 5;
+        String s = "Hello";
+        String space = " ";
+        String name = "Bob";
+        System.out.println(s+space+name);
+        System.out.println("Hello+' '+ John");
+        System.out.println("My number is "+x);
         }
    }
