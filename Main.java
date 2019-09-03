165b66a084b59797df5ba9955a93f391f893d628
daryasovpel <dariawoow56@gmail.com>
2019-09-03 14:17:43 +0300

    Проверка условий

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args){
         int i;
+        int j = 5;
         i =10+2*3;
+        j =j+1;
         System.out.println("i=" +i);
-    }
+        System.out.println("j=" +j);
+        if (i == 16) {
+           System.out.println("i equal 16");
+        } else {
+            System.out.println("i NOT equal 16");
+        }
+   }
 }
