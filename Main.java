33ab0410efe1e48385c29cfe48ed276cd082d18e
daryasovpel <dariawoow56@gmail.com>
2019-09-05 00:52:05 +0300

    Массив, двухмерный массив

--- Main.java
+++ Main.java
@@ -2,27 +2,19 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        int i = 10;
-        while (i >- 0) {
-            System.out.println(i);
-            i -- 3;
-        }
-        //for (int i =8; i>=0; i -= 2) {
-            //if (i == 6) continue;
-            //System.out.println(i);
-       // }
 
-        for (int i =0; i<5; i++) {
-            System.out.println(i);
+        int[] arr = new int[5]; //5-размерность м
+        for (int i = 0; i < 5; i++) {
+            arr[i] = i * 2;
+            System.out.println(arr[i]);
         }
 
-        for (int i =4; i<0; i--) {
-            System.out.println(i);
+        int[] [] arr = new int[5][5]; //5-размерность м
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++)
+                arr [i][j] = i * j;
+            System.out.println(arr[i][j] + " ");
         }
-
-        for (int i =8; i>=0; i -= 2) {
-            System.out.println(i);
-            if (i == 6) break;
-        }
+        System.out.println(" ");
     }
-}
+    }
