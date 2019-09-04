16cdd9cd208a6da140e3e38eff73e6f31b38884e
daryasovpel <dariawoow56@gmail.com>
2019-09-04 17:35:53 +0300

    Switch

--- Main.java
+++ Main.java
@@ -1,15 +1,19 @@
+
 public class Main {
-    public static void main(String[] args){
-        int a =128;
-        int b;
-        System.out.println("a= "+a);
-        b = a/2;
-        //условные операторы  !=(не равно);&&(и)||(или)
-        //==(равно);<(м);>(Б);>=(б,либо равно);<=(м,либо равно);
-        System.out.println("b= "+b);
-        if (a<256)
-        System.out.println("a<256");
-        else
-            System.out.println("a>=256");
+    public static void main(String[] args) {
+        int a = 10;
+        switch (a) {
+            case 10:
+                System.out.println("a10");
+                break;
+            case 15:
+                System.out.println("a15");
+                break;
+            case 20:
+                System.out.println("a20");
+                break;
+            default:
+                System.out.println("Nobody");
         }
-   }
+    }
+}
