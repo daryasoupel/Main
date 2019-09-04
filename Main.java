4ef605a2455c7d3cd8846c195e08f4ef2cfbe3f1
daryasovpel <dariawoow56@gmail.com>
2019-09-04 19:26:35 +0300

    For and While

--- Main.java
+++ Main.java
@@ -1,19 +1,28 @@
+import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        switch (a) {
-            case 10:
-                System.out.println("a10");
-                break;
-            case 15:
-                System.out.println("a15");
-                break;
-            case 20:
-                System.out.println("a20");
-                break;
-            default:
-                System.out.println("Nobody");
+        int i = 10;
+        while (i >- 0) {
+            System.out.println(i);
+            i -- 3;
+        }
+        //for (int i =8; i>=0; i -= 2) {
+            //if (i == 6) continue;
+            //System.out.println(i);
+       // }
+
+        for (int i =0; i<5; i++) {
+            System.out.println(i);
+        }
+
+        for (int i =4; i<0; i--) {
+            System.out.println(i);
+        }
+
+        for (int i =8; i>=0; i -= 2) {
+            System.out.println(i);
+            if (i == 6) break;
         }
     }
 }
