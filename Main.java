94b568b5990d3919dce4513be065e1b1dcdbd8a7
daryasovpel <dariawoow56@gmail.com>
2019-09-05 15:10:27 +0300

    Класс Scanner

--- Main.java
+++ Main.java
@@ -1,20 +1,10 @@
-import java.sql.SQLOutput;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+       Scanner s = new Scanner(System.in);
+       System.out.println("введите что-нибудь");
+       String string = s.nextLine();
+       System.out.println("вы вывели"+string);
 
-        int[] arr = new int[5]; //5-размерность м
-        for (int i = 0; i < 5; i++) {
-            arr[i] = i * 2;
-            System.out.println(arr[i]);
-        }
-
-        int[] [] arr = new int[5][5]; //5-размерность м
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++)
-                arr [i][j] = i * j;
-            System.out.println(arr[i][j] + " ");
-        }
-        System.out.println(" ");
     }
     }
