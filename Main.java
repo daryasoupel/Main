314b31b9ed8b3a8d9953a7b8d9d20fbeb5658c0f
daryasovpel <dariawoow56@gmail.com>
2019-09-05 15:26:11 +0300

    While2

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-       Scanner s = new Scanner(System.in);
-       System.out.println("введите что-нибудь");
-       String string = s.nextLine();
-       System.out.println("вы вывели"+string);
-
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("Введи 5");
+       int value = scanner.nextInt();
+       while (value!=5) {
+           System.out.println("Введи 5");
+           value = scanner.nextInt();
+       }
+       System.out.println("Вы ввели 5");
     }
     }
