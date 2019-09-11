5a8eca09a1c2b2175df7e3b2e36fc2f5a6c947dc
daryasovpel <dariawoow56@gmail.com>
2019-09-11 13:14:24 +0300

    Тип возвращаемого значения метода

--- Main.java
+++ Main.java
@@ -4,17 +4,23 @@ public class Main {
         Person person1 =new Person();
         person1.name = "Роман";
         person1.age = 50;
-        person1.sayHello();
         Person person2 =new Person();
         person2.name = "Вова";
         person2.age = 20;
-        person2.speak();
-        person2.sayHello();
+        int year1 = person1.calculateYearsToRetirement();
+        int year2 = person2.calculateYearsToRetirement();
+        System.out.println("Первому человеку до пенсии: "+year1+" лет");
+        System.out.println("Второму человеку до пенсии: "+year2+" лет");
         }
     }
 class Person{
     String name;
     int age;
+
+    int calculateYearsToRetirement(){
+        int years = 65-age;
+        return years;
+    }
 
     void speak(){
         for(int i=0; i<3; i++);{
